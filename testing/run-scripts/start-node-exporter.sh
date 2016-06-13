ec7b884136db12a1782da667bac6784bceb00ef5
root <lucyhe@google.com>
2016-06-13 15:28:03 -0400

    Add script to start node exporter.

---
+++ testing/run-scripts/start-node-exporter.sh
@@ -0,0 +1,6 @@
+cd
+curl -LO https://github.com/prometheus/node_exporter/releases/download/0.12.0/node_exporter-0.12.0.linux-amd64.tar.gz
+tar -xvzf node_exporter-0.12.0.linux-amd64.tar.gz
+cd node_exporter-0.12.0.linux-amd64/
+./node_exporter
+
