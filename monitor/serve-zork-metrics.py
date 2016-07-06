d09920d6b849d7127d81b438e0498b4f05195f22
Lucy He <lucydhe@gmail.com>
2016-07-06 18:06:54 -0400

    Rename 'connections' to 'getters'

--- monitor/serve-zork-metrics.py
+++ monitor/serve-zork-metrics.py
@@ -4,26 +4,26 @@ import telnetlib
 
 tn = telnetlib.Telnet("zork", "9000")
 
-connections_gauge = Gauge('number_of_connections', 'Number of uProxy instances connected to this cloud instance.')
+getters_gauge = Gauge('number_of_getters', 'Number of uProxy instances connected to this cloud instance.')
 
-def update_connections_count():
+def update_getters_count():
   try:
-    tn.write("connections" + "\n")
+    tn.write("getters" + "\n")
   except e:
     print "Write to zork failed"
     return
   zork_output = tn.read_some()
   # Remove new line character from output and cast to int
-  num_of_connections = int(zork_output[:len(zork_output)-1])
+  num_of_getters = int(zork_output[:len(zork_output)-1])
 
-  connections_gauge.set(num_of_connections)
+  getters_gauge.set(num_of_getters)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
-    
+
     while True:
       # Ping zork every 3 seconds.
       time.sleep(3)
-      update_connections_count()
+      update_getters_count()
 
