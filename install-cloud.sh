f5e3180e24125cb96c0a30cf742c7961ba65269f
Trevor Johnston <trevj@uproxy.org>
2017-01-06 15:11:15 -0500

    add note about passing switches

--- install-cloud.sh
+++ install-cloud.sh
@@ -4,4 +4,12 @@
 # https://github.com/uProxy/uproxy. This script
 # keeps older uProxy clients working.
 
+# Note that this does *not* pass along any arguments.
+# The only downside of this is that older clients will
+# not benefit from the -a switch and will have to wait
+# slightly longer for their access codes to appear due
+# to Node.js being installed.
+
+echo $@
+
 curl https://raw.githubusercontent.com/uProxy/uproxy/master/install-cloud.sh | sh
